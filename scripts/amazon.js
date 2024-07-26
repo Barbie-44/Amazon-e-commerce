d2731753d7e7a5340d23c024bd18bf2e6bcc50b6
Bere <kaamos@ciencias.unam.mx>
2024-07-26 16:22:59 -0600

    Feat: Make cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -78,7 +78,13 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
             quantity: 1
         });
         }
+
+        let cartQuantity = 0;
         
-        console.log(cart);
+
+        cart.forEach((item) => {
+            cartQuantity += item.quantity;
+        });
+        document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
     })
 });
