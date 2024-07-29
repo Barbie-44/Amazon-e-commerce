426b4bb6a179ae78b3b38fc8570ff5ec7ea753d3
Bere <kaamos@ciencias.unam.mx>
2024-07-29 12:34:12 -0600

    Feat: save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,14 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart'))|| [{
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2
 }, {
     productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
     quantity: 1
 }];
+
+function saveToStorage() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 
 export function addToCart(productId){
@@ -25,6 +29,7 @@ export function addToCart(productId){
         quantity: 1
     });
     }
+    saveToStorage();
 }
 
 export function removeFromCart(productId) {
@@ -35,4 +40,5 @@ export function removeFromCart(productId) {
         }
     });
     cart = newCart;
+    saveToStorage();
 };
