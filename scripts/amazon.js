e0708f53daf61740dd691d5fb2ac1882ebcb5cf6
Bere <kaamos@ciencias.unam.mx>
2024-07-29 10:54:04 -0600

    Ref: Build cart module

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,3 +1,5 @@
+import {cart} from '../data/cart.js';
+
 let productsHTML = '';
 
 products.forEach((product) => {
@@ -17,7 +19,7 @@ products.forEach((product) => {
             src="images/ratings/rating-${product.rating.stars*10}.png">
         <div class="product-rating-count link-primary">
             ${product.rating.count}
-        </div>
+        </div> 
         </div>
 
         <div class="product-price">
